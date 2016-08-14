40122ade6bd6367feef0ea27a2b69f1c90785a44
Sean M. Duffy <seanmduffy42@gmail.com>
2016-08-14 12:08:57 -0400

    direct output to file

--- angular2-quickstart/deployScripts/npmInstall.bat
+++ angular2-quickstart/deployScripts/npmInstall.bat
@@ -1,2 +1,2 @@
 CD C:\ProgramData\Amazon\CodeDeploy\%DEPLOYMENT_GROUP_ID%\%DEPLOYMENT_ID%\deployment-archive\angular2-quickstart
-"C:\Program Files\nodejs\npm.cmd" install
+"C:\Program Files\nodejs\npm.cmd" install > npm_log.txt
