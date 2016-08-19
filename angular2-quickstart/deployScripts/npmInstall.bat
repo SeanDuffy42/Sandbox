5053caa38fed7d3a76e24522836199a6872a1361
Sean M. Duffy <seanmduffy42@gmail.com>
2016-08-18 20:55:57 -0400

    test

--- angular2-quickstart/deployScripts/npmInstall.bat
+++ angular2-quickstart/deployScripts/npmInstall.bat
@@ -1,1 +1,2 @@
+CD C:\ProgramData\Amazon\CodeDeploy\%DEPLOYMENT_GROUP_ID%\%DEPLOYMENT_ID%\deployment-archive\angular2-quickstart\deployScripts
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell npmInstall.ps1
